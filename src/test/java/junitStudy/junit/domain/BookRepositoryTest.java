5db0eb833258eb7dcec712402fed41e380f7445e
rnwnsgud <rnwnsgud90@naver.com>
2022-09-14 20:05:53 +0900

    Repository 테스트 환경 세팅 완료

--- src/test/java/junitStudy/junit/domain/BookRepositoryTest.java
+++ src/test/java/junitStudy/junit/domain/BookRepositoryTest.java
@@ -1,7 +1,28 @@
 package junitStudy.junit.domain;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest // DB 와 관련된 컴포넌트만 메모리에 로딩
 class BookRepositoryTest {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    // 1. 책 등록
+    @Test
+    void bookResister() {
+
+    }
+    // 2. 책 목록 보기
+
+    // 3. 책 한건 보기
+
+    // 4. 책 수정
+
+    // 5. 책 삭제
 
 }
