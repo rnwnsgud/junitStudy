3a5c1f12c7822e37d55e86e5effd0e72c23efd12
rnwnsgud <rnwnsgud90@naver.com>
2022-09-15 22:10:40 +0900

    레포지토리 테스트 - 책 삭제하기 버그 수정

--- src/test/java/junitStudy/junit/domain/BookRepositoryTest.java
+++ src/test/java/junitStudy/junit/domain/BookRepositoryTest.java
@@ -1,18 +1,41 @@
 package junitStudy.junit.domain;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest // DB 와 관련된 컴포넌트만 메모리에 로딩
+//@SpringBootTest
+//@Transactional
 class BookRepositoryTest {
 
     @Autowired
     private BookRepository bookRepository;
+
+    //@BeforeAll 테스트 시작 전에 한번만 실행
+    @BeforeEach // 각 테스트 시작 전에 한번씩 실행
+    public void dataSetting() {
+        String title = "junit5";
+        String author = "구준형";
+        Book book = Book.builder()
+                .title(title)
+                .author(author)
+                .build();
+        bookRepository.save(book);
+
+    }
 
     // 1. 책 등록
     @Test
@@ -32,12 +55,46 @@ class BookRepositoryTest {
         assertThat(bookPS.getAuthor()).isEqualTo(author);
 
     }
+
     // 2. 책 목록 보기
+    @Test
+    void findBookList() {
+        //given
+        String title = "junit5";
+        //when
+        List<Book> booksPS = bookRepository.findAll();
+        //then
+        assertThat(booksPS.get(0).getTitle()).isEqualTo(title);
+    }
 
-    // 3. 책 한건 보기
+    // 3. 책 한권 보기
+    @Sql("classpath:db/tableInit.sql")
+    @Test
+    void findOneBook() {
+        //given
+        String title = "junit5";
 
-    // 4. 책 수정
+        //when
+        Book bookPS = bookRepository.findById(1L).get();
 
-    // 5. 책 삭제
+        //then
+        assertThat(bookPS.getTitle()).isEqualTo(title);
+    }
+
+    // 4. 책 삭제
+    @Sql("classpath:db/tableInit.sql")
+    @Test
+    void bookDelete() {
+        //given
+        Long id = 1L;
+
+        //when
+        bookRepository.deleteById(id);
+
+        //then
+        assertFalse(bookRepository.findById(id).isPresent());
+    }
+
+    // 5. 책 수정
 
 }
