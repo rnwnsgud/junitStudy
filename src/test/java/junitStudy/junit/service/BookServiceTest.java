3f32b4ef0650517a7a3424d6407b876cb315b8e5
rnwnsgud <rnwnsgud90@naver.com>
2022-09-17 01:31:20 +0900

    서비스 레이어 테스트 책 등록하기 - stub, mock 완료

--- src/test/java/junitStudy/junit/service/BookServiceTest.java
+++ src/test/java/junitStudy/junit/service/BookServiceTest.java
@@ -1,22 +1,36 @@
 package junitStudy.junit.service;
 
 import junitStudy.junit.domain.BookRepository;
+import junitStudy.junit.util.MailSender;
 import junitStudy.junit.util.MailSenderStub;
 import junitStudy.junit.web.dto.BookRespDto;
 import junitStudy.junit.web.dto.BookSaveReqDto;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
-@DataJpaTest
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+// 가짜 메모리 환경 생성
+@ExtendWith(MockitoExtension.class)
 class BookServiceTest {
 
-    @Autowired
+    @InjectMocks
+    private BookService bookService;
+
+    // 가짜 환경에 띄우기
+    @Mock
     private BookRepository bookRepository;
+
+    @Mock
+    private MailSender mailSender;
 
     @Test
     void resisterBook() {
@@ -25,11 +39,12 @@ class BookServiceTest {
         dto.setTitle("junit강의");
         dto.setAuthor("메타코딩");
 
-        //stub
-        MailSenderStub mailSenderStub = new MailSenderStub();
+        //stub (가설)
+        //실행결과를 미리 정의한다
+        when(bookRepository.save(any())).thenReturn(dto.toEntity());
+        when(mailSender.send()).thenReturn(true);
 
         //when
-        BookService bookService = new BookService(bookRepository, mailSenderStub);
         BookRespDto bookRespDto = bookService.resisterBook(dto);
         //then
         assertThat(bookRespDto.getTitle()).isEqualTo(dto.getTitle());
